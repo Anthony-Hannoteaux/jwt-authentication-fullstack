3a703a99dc25fde1798aca9892ddd2f43300cc01
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2026-04-19 00:03:05 +0200

    Stylization 404 page

--- frontend/src/pages/NotFoundPage/index.jsx
+++ frontend/src/pages/NotFoundPage/index.jsx
@@ -1,5 +1,19 @@
+import "./style.scss"
+
+import { Link } from "react-router-dom"
+
 export default function NotFoundPage() {
     return (
-        <h1>Test 404 Not Found</h1>
+        <main className="main not-found" >
+            <div className="not-found__container">
+                <h1 className="title">Erreur 404</h1>
+                <p>La page que vous recherchez n'existe pas.</p>
+                <Link
+                to={"/"}
+                >
+                    Retour à l'accueil
+                </Link>
+            </div>
+        </main>
     )
 }
