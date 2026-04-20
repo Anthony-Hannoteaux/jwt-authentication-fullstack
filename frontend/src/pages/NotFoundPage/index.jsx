20b3b09656e5f99424b6a6381dd4be1a64095b39
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2026-04-20 23:41:54 +0200

    Inprove a11y for NotFoundPage

--- frontend/src/pages/NotFoundPage/index.jsx
+++ frontend/src/pages/NotFoundPage/index.jsx
@@ -1,19 +1,26 @@
 import "./style.scss"
 
+import PageMeta from "../../Components/meta/PageMeta"
 import { Link } from "react-router-dom"
 
 export default function NotFoundPage() {
     return (
-        <main className="main not-found" >
-            <div className="not-found__container">
-                <h1 className="title">Erreur 404</h1>
-                <p>La page que vous recherchez n'existe pas.</p>
-                <Link
-                to={"/"}
-                >
-                    Retour à l'accueil
-                </Link>
-            </div>
-        </main>
+        <>
+            <PageMeta
+                title="JWT Authentication App - Erreur 404"
+                description="La page demandée est introuvable. Retournez à l'accueil de l'application."
+            />
+            <main className="main not-found" >
+                <section className="not-found__container">
+                    <h1 className="title">Erreur 404</h1>
+                    <p>La page que vous recherchez n'existe pas.</p>
+                    <Link
+                        to={"/"}
+                    >
+                        Retour à l'accueil
+                    </Link>
+                </section>
+            </main>
+        </>
     )
 }
